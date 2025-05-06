c143e314988caf10dde6c33324a74ee5c98eb900
Rajasekar <erajasekargv@gmail.com>
2025-05-06 05:59:51 +0530

    AI-chat-app: request and reponse interface is added

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,8 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { OpenAI } from 'openai';
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export default async function handler(req, res) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
--- pages/api/generate-chat.js
+++ pages/api/generate-chat.js
@@ -1,6 +1,7 @@
 // /pages/api/generate.js or /app/api/generate/route.js (Next.js App Router)
 import axios from 'axios';
 
+
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
